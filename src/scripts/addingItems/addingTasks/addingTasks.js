bba5041bbffd7594b685e79d1514a85a36dcf7d4
dimashpk <shupimonstr@gmail.com>
2020-05-25 16:52:48 +0300

    build of project

--- src/scripts/addingItems/addingTasks/addingTasks.js
+++
@@ -1,6 +0,0 @@
-import { addItems } from "../addingItems";
-import { callBackFunction } from "./callBackFunc";
-
-export const addNewTask = (addingButtons, inputs, arrayOfItems, renderFunc) => {
-  addItems(addingButtons, inputs, arrayOfItems, renderFunc, callBackFunction);
-};
