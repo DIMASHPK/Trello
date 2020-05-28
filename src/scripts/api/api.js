ab34ac4864766bbbd03a0ca481302607a0db091a
dimashpk <shupimonstr@gmail.com>
2020-05-28 16:21:45 +0300

    new deploy

--- src/scripts/api/api.js
+++
@@ -1,9 +0,0 @@
-export const columnsMainApiUrl = `https://trello-82cb9.firebaseio.com/columns.json`;
-export const tasksMainApiUrl = `https://trello-82cb9.firebaseio.com/tasks.json`;
-
-export const columnsApi = (firebaseid) => {
-  return `https://trello-82cb9.firebaseio.com/columns/${firebaseid}.json`;
-};
-export const tasksApi = (firebaseid) => {
-  return `https://trello-82cb9.firebaseio.com/tasks/${firebaseid}.json`;
-};
